862db9cc611d4f2ddce5421a2b7aee41bebd039c
mvxxx <yohama8832@o2.pl>
2017-07-30 00:02:57 +0200

    Methods of configuring the background

--- src/Graphics.cpp
+++ src/Graphics.cpp
@@ -1,42 +1,43 @@
 #include "Graphics.h"
 
-Graphics::Graphics() {
+Graphics::Graphics()
+  :DEFAULT_CLEAR_COLOR( c64::BLACK )
+{
+}
+
+void Graphics::DrawMenu( Player * ) {
 
 }
 
-void Graphics::DrawMenu(Player *) {
-
+void Graphics::SetInnerColor( const sf::Color& color ) {
+  rectangle_screen_.setFillColor( color );
 }
 
-void Graphics::SetInnerColor(sf::Color color) {
-
+void Graphics::SetOuterColor( const sf::Color& color ) {
+  rectangle_border_.setFillColor( color );
 }
 
-void Graphics::SetOuterColor(sf::Color color) {
-
-}
-
-bool Graphics::GetInteractive(int &value, int a, int b, bool clamp) {
+bool Graphics::GetInteractive( int &value, int a, int b, bool clamp ) {
   return false;
 }
 
-bool Graphics::GetInteractive(float &value, float a, float b, bool clamp) {
+bool Graphics::GetInteractive( float &value, float a, float b, bool clamp ) {
   return false;
 }
 
-bool Graphics::GetInteractive(std::string &value, int a, int b, bool clamp) {
+bool Graphics::GetInteractive( const std::string &value, int a, int b, bool clamp ) {
   return false;
 }
 
-void Graphics::Write(std::string text, Graphics::WriteMode mode) {
+void Graphics::Write( const std::string& text, Graphics::WriteMode mode ) {
 
 }
 
-void Graphics::SetClearScreenColor(sf::Color color) {
-
+void Graphics::SetClearScreenColor( const sf::Color& color ) {
+  color_clear_ = color;
 }
 
-void Graphics::CursorMove(int x, int y) {
+void Graphics::CursorMove( int x, int y ) {
 
 }
 
@@ -44,14 +45,14 @@ void Graphics::CursorReset() {
 
 }
 
-void Graphics::SetKeyboardCallback(Callback callback, int keyCode) {
+void Graphics::SetKeyboardCallback( Callback callback, int keyCode ) {
 
 }
 
-void Graphics::CameraChangeScene(int x, int y, bool sliding) {
+void Graphics::CameraChangeScene( int x, int y, bool sliding ) {
 
 }
 
-void Graphics::SceneBuild(Scene::Scene scene, int x, int y) {
+void Graphics::SceneBuild( Scene::Scene scene, int x, int y ) {
 
 }
--- src/Graphics.h
+++ src/Graphics.h
@@ -7,35 +7,50 @@
 #include "colors.h"
 #include "types.h"
 #include "Player.h"
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Font.hpp>
 
 class Graphics {
 public:
-  enum WriteMode {
+  enum class WriteMode {
     BlackOnWhite, WhiteOnBlack
   };
 
   Graphics();
 
-  void DrawMenu(Player*);
+  void DrawMenu( Player* );
   // TODO Draw other stuff like player creation etc
 
-  void SetInnerColor(sf::Color color);
-  void SetOuterColor(sf::Color color);
+  void SetInnerColor( const sf::Color& color );
+  void SetOuterColor( const sf::Color&  color );
 
-  bool GetInteractive(int& value, int a=0, int b=0, bool clamp=false);
-  bool GetInteractive(float& value, float a=0, float b=0, bool clamp=false);
-  bool GetInteractive(std::string& value, int a=0, int b=0, bool clamp=false);
+  bool GetInteractive( int& value, int a = 0, int b = 0, bool clamp = false );
+  bool GetInteractive( float& value, float a = 0, float b = 0, bool clamp = false );
+  bool GetInteractive( const std::string& value, int a = 0, int b = 0, bool clamp = false );
 
-  void Write(std::string text, WriteMode mode = WhiteOnBlack);
-  void SetClearScreenColor(sf::Color color);
+  void Write( const std::string& text, WriteMode mode = WriteMode::WhiteOnBlack );
+  void SetClearScreenColor( const sf::Color& color );
 
-  void CursorMove(int x, int y);
-  void CursorReset();
+  void CursorMove( int x, int y ); /**/
+  void CursorReset(); /**/
 
-  void SetKeyboardCallback(Callback callback, int keyCode);
+  void SetKeyboardCallback( Callback callback, int keyCode );
 
-  void CameraChangeScene(int x, int y, bool sliding=false);
-  void SceneBuild(Scene::Scene scene, int x, int y);
+  void CameraChangeScene( int x, int y, bool sliding = false );
+  void SceneBuild( Scene::Scene scene, int x, int y );
+
+private:
+  const sf::Color DEFAULT_CLEAR_COLOR;
+
+  sf::RenderWindow window_;
+  sf::View view_;
+  sf::Font font_commodore_;
+
+  sf::RectangleShape rectangle_screen_;
+  sf::RectangleShape rectangle_border_;
+
+  sf::Color color_clear_;;
 };
 
 
